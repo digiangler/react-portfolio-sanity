4fa5973d15557c0d84cbeb60f818f215df6bbd88
Hiroshi Yamamoto <digiangler777@gmail.com>
2021-02-28 00:55:05 +0900

    🔧 Add the Home, Post configuration file

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-import sanityClient from "@sanity/cli";
+import sanityClient from "@sanity/client";
 
 export default sanityClient({
   projectId: "wnqn685e",
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+import image from "../images/bg.png";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main>
+      <img
+        src={image}
+        alt="Digi-Angler"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-20 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Full-Stack Engineer
+        </h1>
+      </section>
+    </main>
   );
 }
 
